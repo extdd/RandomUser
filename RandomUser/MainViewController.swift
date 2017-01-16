cb50999010cb675cbaa82a8a1379c898e215403f
extdd <contact@extended.pl>
2017-01-16 22:30:24 +0100

    Create MainViewController

---
+++ RandomUser/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  RandomUser
+//
+//  Created by Krzysztof Ignac on 16.01.2017.
+//  Copyright © 2017 EXTENDED. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+    }
+
+}
