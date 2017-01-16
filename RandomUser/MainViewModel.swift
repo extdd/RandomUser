9d2133b65788ddad392a12bf3e6de0ba7fee20b5
extdd <contact@extended.pl>
2017-01-16 22:30:33 +0100

    Create MainViewModel

---
+++ RandomUser/MainViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewModel.swift
+//  RandomUser
+//
+//  Created by Krzysztof Ignac on 16.01.2017.
+//  Copyright © 2017 EXTENDED. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - INTERFACE
+
+protocol MainViewModel {
+    
+}
+
+// MARK: - IMPLEMENTATION
+
+struct MainViewModelImpl: MainViewModel {
+
+}
