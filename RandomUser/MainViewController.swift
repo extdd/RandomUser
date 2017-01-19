06740de242933d6851dc1055d58df0f345527e1d
extdd <contact@extended.pl>
2017-01-19 16:31:39 +0100

    Implement RxSwift

--- RandomUser/MainViewController.swift
+++ RandomUser/MainViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 enum SortingMode: String {
     
@@ -20,6 +22,7 @@ class MainViewController: UIViewController {
     let navigationBarTitle = "Users" // title of the NavigationBar
     let cellId = "mainTableViewCell" // unique id of a TableView cell
     let padding:CGFloat = 8
+    let disposeBag = DisposeBag()
     
     var mainViewModel:MainViewModel?
     var apiManager:APIManager?
@@ -28,19 +31,51 @@ class MainViewController: UIViewController {
     var tableView:UITableView?
     var sortingControl:UISegmentedControl?
     
+    // MARK: - INIT
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         initUI()
+        initRX()
         mainViewModel?.initData()
         
     }
     
-    // MARK: - ACTIONS
+    // MARK: - TABLE VIEW DATA SOURCE
     
-    func add() {
+    func updateTableData(sorted:SortingMode){
         
-        print("add")
+        print("sorted: \(sorted.rawValue)")
+        
+    }
+    
+    // MARK: - RX
+    
+    fileprivate func initRX() {
+        
+        // buttonAdd
+        
+        buttonAdd?.rx.tap.subscribe(onNext: { _ in
+            
+            print("add")
+            
+        }).addDisposableTo(disposeBag)
+        
+        // sortingControl
+        
+        sortingControl?.rx.value.subscribe(onNext: { [weak self] value in
+            
+            switch (value) {
+            case 0:
+                self?.updateTableData(sorted: SortingMode.firstName)
+            case 1:
+                self?.updateTableData(sorted: SortingMode.lastName)
+            default:
+                return
+            }
+            
+        }).addDisposableTo(disposeBag)
         
     }
     
@@ -59,7 +94,7 @@ class MainViewController: UIViewController {
     
     fileprivate func initNavigationBar() {
         
-        buttonAdd = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add))
+        buttonAdd = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
         navigationItem.rightBarButtonItems = [buttonAdd!]
         
     }
@@ -72,11 +107,9 @@ class MainViewController: UIViewController {
         self.view.addSubview(sortingControl!)
         
         sortingControl!.snp.makeConstraints { make in
-            
             make.top.equalTo(self.topLayoutGuide.snp.bottom).offset(padding)
             make.width.lessThanOrEqualTo(self.view).inset(padding)
             make.centerX.equalToSuperview()
-            
         }
         
     }
@@ -88,10 +121,8 @@ class MainViewController: UIViewController {
         self.view.addSubview(tableView!)
         
         tableView!.snp.makeConstraints { make in
-            
             make.top.equalTo(sortingControl!.snp.bottom).offset(padding)
             make.bottom.left.right.equalTo(self.view)
-            
         }
         
     }
