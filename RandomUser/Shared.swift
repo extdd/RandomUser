49ef522db249bd24f1bc3e9724511282220df02c
extdd <contact@extended.pl>
2017-01-25 20:23:05 +0100

    Add shared constants

--- RandomUser/Shared.swift
+++ RandomUser/Shared.swift
@@ -8,8 +8,32 @@
 
 import Foundation
 
+// LAYOUT
+
+struct Layout {
+    
+    static let padding = 8
+
+}
+
+// COLORS
+
+struct CustomColor {
+    
+    static let teal = 0x20fec0
+    static let violet = 0x5e1fe4
+    static let violetDark = 0x2d2b37
+    static let gray = 0x939294
+    
+}
+
+// ERRORS
+
 func printError(_ error:Error) {
     
     print("Error: \(error)")
     
 }
+
+
+
