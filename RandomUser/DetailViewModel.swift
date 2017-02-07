68a273ecee141a4a3566eec6e1ffc3593daed80b
extdd <contact@extended.pl>
2017-02-07 22:05:47 +0100

    Create DetailViewModel

---
+++ RandomUser/DetailViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewModel.swift
+//  RandomUser
+//
+//  Created by Krzysztof Ignac on 05.02.2017.
+//  Copyright © 2017 EXTENDED. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - INTERFACE
+
+protocol DetailViewModel {
+    
+    var activeUser: User? {get set}
+    
+}
+
+// MARK: - IMPLEMENTATION
+
+struct DetailViewModelImpl: DetailViewModel {
+
+    var activeUser: User?
+    
+}
